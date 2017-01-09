67ea7b4e29480a8367e32583be2bd8b9e0352f6a
Michael Bowman <bowmanmc@gmail.com>
2017-01-09 15:06:37 -0500

    caliper logo

--- src/app/components/navbar/NavbarTemplate.js
+++ src/app/components/navbar/NavbarTemplate.js
@@ -6,10 +6,10 @@ const NavbarTemplate = () => {
     return (
         <div className="navigation">
             <div className="navigation__logo">
-                <Link to="/"><img src="/images/logo_color.svg" alt="Flock"/></Link>
+                <Link to="/"><img src="/images/caliper.svg" alt="Caliper"/></Link>
             </div>
             <nav className="navigation__navbar">
-                <IndexLink to="/"><i className="fa fa-user"/>Profiles</IndexLink>
+                <IndexLink to="/"><i className="fa fa-user"/>Home</IndexLink>
                 <Link to="/about"><i className="fa fa-info"/>About</Link>
             </nav>
         </div>
