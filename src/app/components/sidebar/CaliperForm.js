8b1f55736490e25fe54740cdb3babefa2b5684c7
Michael Bowman <bowmanmc@gmail.com>
2017-01-10 14:51:45 -0500

    datetime picker

--- src/app/components/sidebar/CaliperForm.js
+++ src/app/components/sidebar/CaliperForm.js
@@ -1,5 +1,9 @@
 import CaliperFormTemplate from './CaliperFormTemplate';
+import Moment from 'moment';
+import momentLocalizer from 'react-widgets/lib/localizers/moment';
 
+console.log('momentLocalizer()');
+momentLocalizer(Moment);
 
 const CaliperForm = (props) => {
   return CaliperFormTemplate(props);
--- src/app/components/sidebar/CaliperFormTemplate.js
+++ src/app/components/sidebar/CaliperFormTemplate.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import DateTimePicker from 'react-widgets/lib/DateTimePicker';
 
 
 const CaliperFormTemplate = () => {
     return (
         <div className="caliper-form">
-            hello.
+            <div className="ais-dtpicker">
+                <div className="ais-dtpicker__label">
+                    <label>Start Time</label>
+                </div>
+                <div className="ais-dtpicker__date">
+                    <DateTimePicker time={false} format={'MM-DD-YYYY'} />
+                </div>
+                <div className="ais-dtpicker__time">
+                    <DateTimePicker calendar={false} timeFormat={'HH:mm [Z]'} format={'HH:mm [Z]'} />
+                </div>
+            </div>
         </div>
     );
 };
