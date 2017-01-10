7420d0882d03eafec107c46270957016ed0ad1fe
ericsvendsen <esvendsen@gmail.com>
2017-01-10 15:02:04 -0500

    adding styles for grid layout

--- src/app/components/HomePage.js
+++ src/app/components/HomePage.js
@@ -10,6 +10,48 @@ const HomePage = () => {
                 developed for NGA Research.
             </p>
 
+            <div className="flex-grid">
+                <div className="col">
+                    Just some
+                </div>
+                <div className="col">
+                    text to show
+                </div>
+                <div className="col">
+                    what a standard
+                </div>
+                <div className="col">
+                    grid looks like.
+                </div>
+            </div>
+
+            <div className="flex-grid-thirds">
+                <div className="col">
+                    Here's a grid
+                </div>
+                <div className="col">
+                    that's separated
+                </div>
+                <div className="col">
+                    into thirds with gutters.
+                </div>
+            </div>
+
+            <div className="flex-grid-quarters">
+                <div className="col">
+                    Here's a
+                </div>
+                <div className="col">
+                    grid that's
+                </div>
+                <div className="col">
+                    separated into
+                </div>
+                <div className="col">
+                    quarters with gutters.
+                </div>
+            </div>
+
         </div>
     );
 };
