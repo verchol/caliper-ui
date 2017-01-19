bd85d77c941859670df399a793991f11bbdfab44
Michael Bowman <bowmanmc@gmail.com>
2017-01-19 14:57:33 -0500

    Pull Error Criteria checkboxes from app-config.js

--- src/app-config.dev.js
+++ src/app-config.dev.js
@@ -10,5 +10,29 @@ APP_CONFIG = {
         requirementId: 'requirementId_like',
         productId: 'productId_like',
         taskName: 'taskName_like'
+    },
+    form: {
+        criteria: [{
+            name: 'isMonkey',
+            label: 'Is a Monkey'
+        }, {
+            name: 'isGorilla',
+            label: 'Is a Gorilla'
+        }, {
+            name: 'isLemur',
+            label: 'Is a Lemur'
+        }, {
+            name: 'isOrangutan',
+            label: 'Is an Orangutan'
+        }, {
+            name: 'isGibbon',
+            label: 'Is a Gibbon'
+        }, {
+            name: 'isHuman',
+            label: 'Is a Human'
+        }, {
+            name: 'isChimpanzee',
+            label: 'Is a Chimpanzee'
+        }]
     }
 };
--- src/app/components/sidebar/sections/ErrorCriteriaSection.js
+++ src/app/components/sidebar/sections/ErrorCriteriaSection.js
@@ -1,10 +1,43 @@
-import ErrorCriteriaSectionTemplate from './ErrorCriteriaSectionTemplate';
+import React from 'react';
+
+import AisCheckbox from '../form/AisCheckbox';
 
 
-const ErrorCriteriaSection = () => {
+class ErrorCriteriaSection extends React.Component {
 
-    return ErrorCriteriaSectionTemplate();
+    constructor(props, context) {
+        super(props, context);
 
-};
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange() {
+        console.log('handling Error Criteria checkbox change!');
+    }
+
+    render() {
+
+        let criteria = APP_CONFIG.form.criteria;
+        let changeHandler = this.handleChange;
+
+        return (
+            <section>
+                <h2>Error Criteria</h2>
+                <fieldset>
+                {
+                    criteria.map(function(criterion){
+                        return (
+                            <AisCheckbox key={criterion.name}
+                                name={criterion.name}
+                                label={criterion.label}
+                                onChange={changeHandler} />
+                        )
+                    })
+                }
+                </fieldset>
+            </section>
+        );
+    }
+}
 
 export default ErrorCriteriaSection;
