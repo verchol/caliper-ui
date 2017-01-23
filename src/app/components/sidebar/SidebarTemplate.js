bcd6a7f98aee9355632b6b6230d565034967be8d
Michael Bowman <bowmanmc@gmail.com>
2017-01-23 14:34:57 -0500

    alternative fact: flexbox was the culprit

--- src/app/components/sidebar/SidebarTemplate.js
+++ src/app/components/sidebar/SidebarTemplate.js
@@ -6,13 +6,13 @@ import CaliperForm from './CaliperForm';
 
 const SidebarTemplate = () => {
     return (
-        <div className="sidebar">
-            <div className="sidebar__logo">
-                <Link to="/"><img src="/images/caliper.svg" alt="Caliper"/></Link>
-            </div>
+            <div className="sidebar">
+                <div className="sidebar__logo">
+                    <Link to="/"><img src="/images/caliper.svg" alt="Caliper"/></Link>
+                </div>
 
-            <CaliperForm />
-        </div>
+                <CaliperForm />
+            </div>
     );
 };
 
