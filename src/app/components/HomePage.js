5ff11337c7366af76f8c9241323d31bbffc14715
Michael Bowman <bowmanmc@gmail.com>
2017-01-23 15:52:04 -0500

    making the datagrid fill the top half of space

--- src/app/components/HomePage.js
+++ src/app/components/HomePage.js
@@ -5,33 +5,19 @@ import LineChart from './chart/LineChart';
 
 const HomePage = () => {
     return (
-        <div className="page">
-
-            <div className="homepage">
-
-                <h1>Caliper Home Page</h1>
-                <p>
-                    Caliper is an error metric tracking application
-                    developed for NGA Research.
-                </p>
-
-                <div className="homepage__datagrid">
-                    <Datagrid/>
-                </div>
-
-                <div className="homepage__datagrid-pager">
-                    <DatagridPager/>
-                </div>
-
-                <div className="homepage__charts">
-                    <div className="line">
-                    </div>
-                    <div className="histogram">
-                    </div>
-                </div>
-
+        <div className="homepage">
+            <div className="homepage__datagrid">
+                <Datagrid/>
             </div>
-
+            <div className="homepage__datagrid-pager">
+                <DatagridPager/>
+            </div>
+            <div className="homepage__charts">
+                <div className="line">
+                </div>
+                <div className="histogram">
+                </div>
+            </div>
         </div>
     );
 };
